fb50e9e601a4fe53beba4a4b5def92dee007f5ea
Laura Hope <ssteamlaura@gmail.com>
2023-05-10 10:39:30 -0700

    Add properties CityName, Picture, HowLongToStay, TraveledWith with passing tests

--- PlacesYouveBeen.Tests/ModelTests/PlaceTests.cs
+++ PlacesYouveBeen.Tests/ModelTests/PlaceTests.cs
@@ -13,6 +13,38 @@ namespace PlacesYouveBeen.Tests
     {
       Assert.AreEqual(typeof(Place), newPlace.GetType());
     }
-    
+
+    [TestMethod]
+    public void GetCityName_ReturnsCityName_String()
+    {
+      string cityName = "Seattle";
+      newPlace.CityName = cityName;
+      string result = newPlace.CityName;
+      Assert.AreEqual(cityName, result);
+    }
+
+    [TestMethod]
+    public void GetPicture_ReturnsPicture_String()
+    {
+      string picture = "seattle.jpg";
+      newPlace.Picture = picture;
+      Assert.AreEqual(newPlace.Picture, picture);
+    }
+
+    [TestMethod]
+    public void GetHowLongToStay_ReturnsHowLongToStay_String()
+    {
+      string howLongToStay = "3 days";
+      newPlace.HowLongToStay = howLongToStay;
+      Assert.AreEqual(newPlace.HowLongToStay, howLongToStay);
+    }
+
+    [TestMethod]
+    public void GetTraveledWith_ReturnsTraveledWith_String()
+    {
+      string traveledWith = "myself";
+      newPlace.TraveledWith = traveledWith;
+      Assert.AreEqual(newPlace.TraveledWith, traveledWith);
+    }
   }
 }
